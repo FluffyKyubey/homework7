cbbf34d8a0099d2b84793d67d04f819b78528063
John Hull <john.alex.hull@gmail.com>
2022-05-22 23:13:10 -0700

    minor bugs

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const questions = [
 		type: 'input',
 		message: 'What is your application used for?',
 		name: 'usage',
-		// default: 'Gaining EXP with Node',
+		// default: 'Doing stuff',
 	},
 	{
 		type: 'checkbox',
@@ -63,3 +63,84 @@ const questions = [
 		// default: 'john.alex.hull@gmail.com',
 	},
 ];
+
+const createReadme = (data) => {
+		var license = '';
+		var licenseTXT = '';
+		switch (data.license[0]) {
+			case 'BSD':
+				license =
+					'[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+				licenseTXT = `[BSD License](https://opensource.org/licenses/BSD-3-Clause)`;
+				break;
+			case 'MIT':
+				license =
+					'[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+				licenseTXT = `[MIT License](https://opensource.org/licenses/MIT)`;
+				break;
+			case 'GPL':
+				license =
+					'[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+				licenseTXT = `[GPL License](https://www.gnu.org/licenses/gpl-3.0)`;
+	
+				break;
+			default:
+				license = 'No License Chosen';
+				licenseTXT = ``;
+	
+				break;
+		}
+	
+	fs.writeFileSync(
+		'./readme.md',
+		`# ${data.project}
+${license}
+## Table of Contents
+- [Description](#description)
+- [Application Preview](#application-preview)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Test Instructions](#test-instructions)
+- [Questions](#questions)
+- [License](#license)
+## Description
+- ${data.description}
+## Application Preview
+<p align="left">
+<img alt="README Gen Demo 1" src="${data.preview}">
+</p>
+## Installation
+- ${data.install}
+## Usage
+- ${data.usage}
+## Contributing
+- ${data.contribute}
+## Test Instructions
+- ${data.test}
+## Questions
+Questions? Concerns?  Contact Me Below:
+- Github Username: ${data.github}
+- Github Link: https://github.com/${data.github} 
+- Email: ${data.email}
+## License
+- Licensed under the: ${licenseTXT} 
+`
+	);
+};
+		
+
+inquirer
+	.prompt(questions)
+	.then((answers) => {
+		createReadme(answers)
+		console.log(`It worked!`)
+	})
+	.catch((error) => {
+		if (error.isTtyError) {
+			console.error(`Prompts no renedering properly`)
+		}
+		else {
+			console.error(`something went wrong`, error)
+		}
+	})
